6dc278ed7597e7dc7dd489f78e108f2f949c1474
Artem <artem.volkonitin@yandex.ru>
2023-07-12 16:11:09 +0300

    poh

---
+++ src/routes/+page.svelte
@@ -0,0 +1,84 @@
+<svelte:head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello my name is...</title>
+    <link rel="stylesheet" href="style_hello.css">
+</svelte:head>
+
+<header>
+    <div class="container">
+        <div class="navig">
+            <nav>
+                <div class="point-of-nav">
+                    <p class="nav-text">About me</p>
+                    <p class="nav-text">About me</p>
+                    <p class="nav-text">About me</p>
+                    <p class="nav-text-main"><a href="#about" style="color: rgb(255, 106, 0)">About me</a></p>
+                </div>
+                <div class="point-of-nav">
+                    <p class="nav-text">My skills</p>
+                    <p class="nav-text">My skills</p>
+                    <p class="nav-text">My skills</p>
+                    <p class="nav-text-main"><a href="#skills" style="color: rgb(255, 106, 0)">My skills</a></p>
+                </div>
+                <div class="point-of-nav">
+                    <p class="nav-text">Contacts</p>
+                    <p class="nav-text">Contacts</p>
+                    <p class="nav-text">Contacts</p>
+                    <p class="nav-text-main"><a href="#contacts" style="color: rgb(255, 106, 0)">Contacts</a></p>
+                </div>
+            </nav>
+        </div>
+        <div class="lines">
+            <hr class="short">
+            <hr class="short">
+            <hr class="short">
+            <hr class="short">
+            <hr class="short">
+            <hr class="short">
+            <hr class="short">
+            <hr class="short">
+            <hr class="long">
+        </div>
+        <div class="box">
+            <div class="hello-text">
+                <h1 class="hello">Hello!<br>My name is</h1>
+                <h1 id="name">Artem</h1>
+            </div>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="container">
+        <h1 class="hello" id="hello-orange"><a id="about">And I'm a Front-end developer</a></h1>
+        <div class="about-me">
+            <p>I am a first year student of Innopolis University<br>
+                In my courses I am studying how to make awesome web-sites<br>
+                I will be happy to make some web-sites for you</p>
+            <p id="free">FOR FREE!!!</p>
+        </div>
+    </div>
+    <div id="main-skills">
+        <div class="skills">
+            <h1 class="hello" id="my-skills"><a id="skills">My skills</a></h1>
+            <div class="list">
+                <ul>
+                    <li class="skill">HTML</li>
+                    <li class="skill">CSS</li>
+                    <li class="skill">JavaScript</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="surprise">
+        <h2 class="last">I have something for you...</h2>
+        <h3 class="last">Press the button below...</h3>
+        <button id="smth-button" onclick="window.location.href='comic_page'">Something</button>
+    </div>
+</main>
+<footer id="contacts">
+    <a href="https://github.com/UTKANOS-RIBA" id="gh-link"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github logo" width="50"></a>
+    <a href="https://t.me/artem_volkonitin" id="tg-link"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2048px-Telegram_logo.svg.png" alt="telegram logo" width="50"></a>
+    <a href="mailto:a.volkonitin@innopolis.university" id="email"><img src="https://cdn0.iconfinder.com/data/icons/apple-apps/100/Apple_Mail-512.png" alt="email" width="50"></a>
+</footer>
---
+++ src/routes/comic_page/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import dayjs from "dayjs";
+    import type { XKCDObject } from "../../lib/interface";
+    async function getXKCD() {
+            const url: string = `https://fwd.innopolis.app/api/hw2/?${new URLSearchParams({email: 'a.volkonitin@innopolis.university'})}`;
+            const response: Response = await fetch(url);
+            const id: string = await response.text();
+
+            const request: string = `https://fwd.innopolis.university/api/comic?${new URLSearchParams({id})}`;
+            const response2: Response = await fetch(request);
+
+            return await response2.json() as XKCDObject;
+
+    }
+
+    async function setXKCDData() {
+        const jsonData : XKCDObject = await getXKCD();
+       const photoElement : HTMLImageElement = <HTMLImageElement>document.getElementById('photo');
+        const dateElement : HTMLElement = <HTMLElement>document.getElementById('date');
+       photoElement.src = jsonData.img;
+        photoElement.alt = jsonData.alt;
+        photoElement.title = jsonData.safe_title;
+        dateElement.textContent = 'Date of publishing: ' + dayjs(jsonData.year.toString() + '-' + jsonData.month.toString() + '-' + jsonData.day.toString()).toDate();
+    }
+</script>
+
+<svelte:head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Something strange</title>
+    <link rel="stylesheet" href="../style_comic.css">
+</svelte:head>
+<div class="container">
+    <img src="" alt="" id="photo">
+    <div class="in-col">
+        <p id="date"></p>
+        <button id="joke-button" class="button" on:click={setXKCDData} >Get something</button>
+        <button id="back-button" class="button" onclick="window.location.href='../'">Go back</button>
+    </div>
+</div>
